f24c0d4fc0e6fe9a030c1d928749b815827bbf40
NoblesS1x <s20120229@alumnos.itsur.edu.mx>
2023-09-07 00:04:24 -0600

    Version terminada del dice roler app

--- DiceRoller/app/src/main/java/net/ivanvega/diceroller/MainActivity.kt
+++ DiceRoller/app/src/main/java/net/ivanvega/diceroller/MainActivity.kt
@@ -45,7 +45,10 @@ fun DiceWithButtonAndImage(modifier: Modifier = Modifier){
     var result by remember {
         mutableStateOf(1)
     }
-
+    var result2 by remember {
+        mutableStateOf(1)
+    }
+    /*
     val imageResource = when(result){
         1 -> R.drawable.dice_1
          2 -> R.drawable.dice_2
@@ -54,23 +57,54 @@ fun DiceWithButtonAndImage(modifier: Modifier = Modifier){
         5 -> R.drawable.dice_5
         else -> R.drawable.dice_6
     }
+    */
+
+    //@Composable
+    fun roll(result:Int):Int{
+
+        return  when(result){
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+    }
 
     Column(
         modifier = modifier,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Image(painter = painterResource(id = imageResource) ,
+        /*Image(painter = painterResource(id = imageResource) ,
             contentDescription = stringResource(
             R.string.imagen_de_dado
             )
         )
+        */
+        Image(painter = painterResource(id = roll(result = result)) ,
+            contentDescription = stringResource(
+                R.string.imagen_de_dado
+            )
+        )
+        Image(painter = painterResource(id = roll(result = result2)) ,
+            contentDescription = stringResource(
+                R.string.imagen_de_dado
+            )
+        )
+
+        /*
         Spacer(modifier = Modifier.height(16.dp))
         Button(onClick = { result = (1..6).random() }) {
             Text(stringResource(R.string.roll))
         }
+        */
+        Spacer(modifier = Modifier.height(16.dp))
+        Button(onClick = { result = (1..6).random()
+            result2 = (1..6).random()}) {
+            Text(stringResource(R.string.roll))
+        }
     }
-
-
 }
 
 @Preview
