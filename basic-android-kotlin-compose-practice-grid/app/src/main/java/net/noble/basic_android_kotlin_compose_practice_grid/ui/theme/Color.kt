b72d291452172b8a515c7409d95e8920bf45334d
NoblesS1x <s20120229@alumnos.itsur.edu.mx>
2023-09-28 21:21:15 -0600

    Avance y termino de la practica

--- basic-android-kotlin-compose-practice-grid/app/src/main/java/net/noble/basic_android_kotlin_compose_practice_grid/ui/theme/Color.kt
+++ basic-android-kotlin-compose-practice-grid/app/src/main/java/net/noble/basic_android_kotlin_compose_practice_grid/ui/theme/Color.kt
@@ -2,6 +2,15 @@ package net.noble.basic_android_kotlin_compose_practice_grid.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
+val primary_dark = Color(0xFFD0BCFF)
+val secondary_dark = Color(0xFFCCC2DC)
+val tertiary_dark = Color(0xFFEFB8C8)
+
+val primary_light = Color(0xFF6650a4)
+val secondary_light = Color(0xFF625b71)
+val tertiary_light = Color(0xFF7D5260)
+
+/*
 val Purple80 = Color(0xFFD0BCFF)
 val PurpleGrey80 = Color(0xFFCCC2DC)
 val Pink80 = Color(0xFFEFB8C8)
@@ -9,3 +18,4 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+*/
--- basic-android-kotlin-compose-practice-grid/app/src/main/java/net/noble/basic_android_kotlin_compose_practice_grid/ui/theme/Theme.kt
+++ basic-android-kotlin-compose-practice-grid/app/src/main/java/net/noble/basic_android_kotlin_compose_practice_grid/ui/theme/Theme.kt
@@ -16,16 +16,54 @@ import androidx.compose.ui.platform.LocalView
 import androidx.core.view.WindowCompat
 
 private val DarkColorScheme = darkColorScheme(
+    primary = primary_dark,
+    secondary = secondary_dark,
+    tertiary = tertiary_dark
+)
+
+private val LightColorScheme = lightColorScheme(
+    primary = primary_light,
+    secondary = secondary_light,
+    tertiary = tertiary_light
+)
+@Composable
+fun CoursesTheme(
+    darkTheme: Boolean = isSystemInDarkTheme(),
+    // Dynamic color is available on Android 12+
+    dynamicColor: Boolean = false,
+    content: @Composable () -> Unit
+) {
+    val colorScheme = when {
+        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
+            val context = LocalContext.current
+            if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
+        }
+
+        darkTheme -> DarkColorScheme
+        else -> LightColorScheme
+    }
+    val view = LocalView.current
+    if (!view.isInEditMode) {
+        SideEffect {
+            val window = (view.context as Activity).window
+            window.statusBarColor = colorScheme.primary.toArgb()
+            WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = darkTheme
+        }
+    }
+
+/*
+private val DarkColorScheme = darkColorScheme(
     primary = Purple80,
     secondary = PurpleGrey80,
     tertiary = Pink80
 )
-
+ */
+/*
 private val LightColorScheme = lightColorScheme(
     primary = Purple40,
     secondary = PurpleGrey40,
     tertiary = Pink40
-
+ */
     /* Other default colors to override
     background = Color(0xFFFFFBFE),
     surface = Color(0xFFFFFBFE),
@@ -34,9 +72,10 @@ private val LightColorScheme = lightColorScheme(
     onTertiary = Color.White,
     onBackground = Color(0xFF1C1B1F),
     onSurface = Color(0xFF1C1B1F),
-    */
-)
 
+)
+*/
+/*
 @Composable
 fun BasicandroidkotlincomposepracticegridTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
@@ -61,7 +100,7 @@ fun BasicandroidkotlincomposepracticegridTheme(
             WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = darkTheme
         }
     }
-
+ */
     MaterialTheme(
         colorScheme = colorScheme,
         typography = Typography,
