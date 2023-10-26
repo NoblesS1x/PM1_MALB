e3b6790764d3e5cabc04bc8397d5f87883ab864c
NoblesS1x <s20120229@alumnos.itsur.edu.mx>
2023-10-26 00:31:30 -0600

    proyecto terminado

--- basic-android-kotlin-compose-training-reply-app-nav-update/app/src/androidTest/java/com/example/reply/test/TestAnnotations.kt
+++ basic-android-kotlin-compose-training-reply-app-nav-update/app/src/androidTest/java/com/example/reply/test/TestAnnotations.kt
@@ -13,16 +13,8 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+package com.example.reply.test
 
-// Top-level build file where you can add configuration options common to all sub-projects/modules.
-buildscript {
-    extra.apply {
-        set("lifecycle_version", "2.6.1")
-    }
-}
-
-plugins {
-    id("com.android.application") version "8.1.2" apply false
-    id("com.android.library") version "8.1.2" apply false
-    id("org.jetbrains.kotlin.android") version "1.8.21" apply false
-}
+annotation class TestCompactWidth
+annotation class TestMediumWidth
+annotation class TestExpandedWidth
