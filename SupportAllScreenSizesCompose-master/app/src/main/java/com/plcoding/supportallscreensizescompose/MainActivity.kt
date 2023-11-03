7f73dc40f2814a2fe813cd0afba43f37c50e5bce
NoblesS1x <s20120229@alumnos.itsur.edu.mx>
2023-11-03 00:43:20 -0600

    Avance allscreens

--- SupportAllScreenSizesCompose-master/app/src/main/java/com/plcoding/supportallscreensizescompose/MainActivity.kt
+++ SupportAllScreenSizesCompose-master/app/src/main/java/com/plcoding/supportallscreensizescompose/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
@@ -24,6 +25,8 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             SupportAllScreenSizesComposeTheme {
+                val windowInfo = rememberWindowInfo()
+                if(windowInfo.screenWidthInfo is WindowInfo.WindowType.Compact) {
                 LazyColumn(
                     modifier = Modifier.fillMaxSize()
                 ) {
@@ -50,7 +53,46 @@ class MainActivity : ComponentActivity() {
                         )
                     }
                 }
+            }else{
+                Row(modifier = Modifier.fillMaxWidth()){
+                    LazyColumn(
+                        modifier = Modifier.weight(1f)
+                    ) {
+                        // List 1
+                        items(10) {
+                            Text(
+                                text = "Item $it",
+                                fontSize = 25.sp,
+                                modifier = Modifier
+                                    .fillMaxWidth()
+                                    .background(Color.Green)
+                                    .padding(16.dp)
+                            )
+                        }
+
+                    } //lazyend
+                    LazyColumn(
+                        modifier = Modifier.weight(1f)
+                    ) {
+                        // List 1
+                        items(10) {
+                            Text(
+                                text = "Item $it",
+                                fontSize = 25.sp,
+                                modifier = Modifier
+                                    .fillMaxWidth()
+                                    .background(Color.Cyan)
+                                    .padding(16.dp)
+                            )
+                        }
+
+                    } //lazyend
+                }
+
             }
+
         }
     }
+}
+
 }
